ec4cc3887018ae63ca7002b548770048d9cd805e
unknown <marwilsondc1204@gmail.com>
2025-06-27 23:49:43 +0800

    Added exp_up() to class Character

--- classes/character.py
+++ classes/character.py
@@ -1,16 +1,18 @@
 class Character:
     #def __init__
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.base_atk = 17
         self.base_hp = 100
         self.base_mana = 60
         self.level = 1
         self.armor = 0
         self.experience = 0
+        self.overall_exp = 0
 
     #def __str__
     def __str__(self):
-        return f"{self}: Base ATK: {self.base_atk} Base HP: {self.base_hp} Base Mana: {self.base_mana}"
+        return f"{self.name}: Base ATK: {self.base_atk} Base HP: {self.base_hp} Base Mana: {self.base_mana}"
 
     #def attack()
     def attack(self, enemy_armor):
@@ -22,9 +24,9 @@ class Character:
         self.base_hp += change
 
         if change < 0: 
-            print(f"Took {change} damage! {self} Health: {self.base_hp}")
+            print(f"Took {change} damage! {self.name} Health: {self.base_hp}")
         else: 
-            print(f"Got healed {self.base_hp} points! {self} Health: {self.base_hp}")
+            print(f"Got healed {self.base_hp} points! {self.name} Health: {self.base_hp}")
 
 
     #def mana()
@@ -32,12 +34,20 @@ class Character:
         self.base_mana += change
         
         if change < 0: 
-            print(f"Spent {change} mana! {self} mana: {self.base_mana}")
+            print(f"Spent {change} mana! {self.name} mana: {self.base_mana}")
 
         else:
-            print(f"Granted {change} mana! {self} mana: {self.base_mana}")
+            print(f"Granted {change} mana! {self.name} mana: {self.base_mana}")
 
-    #def level_up()
-    def level_up(self):
-        pass
-    #def 
+    #def exp_up()
+    def exp_up(self, change):
+        self.experience += change
+        self.overall_exp += change
+
+        if self.experience >= 50:
+            self.experience = self.experience - 50
+            self.level += 1
+            print(f"{self.name} leveled up to LVL {self.level}!")
+        
+        else:
+            print(f"{self.name} gained {change} EXP!")
