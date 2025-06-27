ea0241b546e08bbf22ed229fe7308dd1d8ace51c
unknown <marwilsondc1204@gmail.com>
2025-06-28 01:34:36 +0800

    Added __init__, __str__, and attack() methods to class Warrior

--- classes/warrior.py
+++ classes/warrior.py
@@ -3,4 +3,24 @@ import random
 from character import Character
 
 class Warrior(Character):
+    def __init__(self, name = "Bob"):
+        super().__init__()
+        self.name = name
+
     
+    def __str__(self):
+        return f"""
+Name: {self.name}
+Class: Warrior
+Level: {self.level}
+ATK: {self.base_atk}
+HP: {self.base_hp}
+MANA: {self.base_mana}
+Armor: {self.armor}
+Exp.: {self.overall_exp}
+CritRate: {self.crit_rate}
+CritDmg: {self.crit_dmg}
+"""
+    
+    def attack():
+        pass
