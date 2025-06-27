0fb28bddce94a6761ab6baf827dbde6e254613a4
unknown <marwilsondc1204@gmail.com>
2025-06-28 01:27:55 +0800

    Added critical hit system and defined appropriate abstract methods in class Character. Added __str__ and attack() method in class Enemy. Uploaded warrior.py and created class Warrior

--- classes/character.py
+++ classes/character.py
@@ -1,23 +1,28 @@
+from abc import ABC, abstractmethod
+
 class Character:
     #def __init__
     def __init__(self, name):
         self.name = name
+        self.level = 0
         self.base_atk = 17
-        self.base_hp = 100
+        self.base_hp = 100 
         self.base_mana = 60
-        self.level = 1
         self.armor = 0
         self.experience = 0
         self.overall_exp = 0
+        self.crit_rate = 15
+        self.crit_dmg = 0.5
 
     #def __str__
+    @abstractmethod
     def __str__(self):
-        return f"{self.name}: Base ATK: {self.base_atk} Base HP: {self.base_hp} Base Mana: {self.base_mana}"
+        pass
 
     #def attack()
-    def attack(self, enemy_armor):
-        damage = (-self.base_atk) * enemy_armor
-        return damage
+    @abstractmethod
+    def attack():
+        pass
     
     #def health()
     def health(self, change):
@@ -47,6 +52,10 @@ class Character:
         if self.experience >= 50:
             self.experience = self.experience - 50
             self.level += 1
+            self.base_atk = 17 + (self.level * 3)
+            self.base_hp = 100 + (self.level * 15)
+            self.base_mana = 60 + (self.level * 5)
+
             print(f"{self.name} leveled up to LVL {self.level}!")
         
         else:
--- classes/enemy.py
+++ classes/enemy.py
@@ -1,6 +1,35 @@
 from character import Character
+from math import ceil
+import random
 
 class Enemy(Character):
     #def __init__(self):
+    def __init__(self, name = None, level = 1, atk = 8, hp = 40, mana = 35, armor = 0, crit_rt = 15, crit_dmg = 0.5):
+        super().__init__()
+        self.name = name 
+        self.level = level
+        self.base_atk = atk
+        self.base_hp = hp
+        self.base_mana = mana
+        self.armor = armor
+        self.crit_rate = crit_rt
+        self.crit_dmg = crit_dmg
 
     #def __str__(self):
+    def __str__(self):
+        return f"""
+Name: {self.name}
+Level: {self.level}"""
+    
+    #def attack()
+    def attack(self, enemy_armor):
+        rand_num_gen = random.randint(1, 100)
+
+        if rand_num_gen <= self.crit_rate:
+            damage = -((self.base_atk + (self.base_atk * self.crit_dmg)) - ceil(enemy_armor * 0.3))
+            print(f"Critical hit! {self.name} did {damage} damage!")
+            return damage
+
+        else:
+            damage = -(self.base_atk - ceil(enemy_armor * 0.35))
+            return damage
