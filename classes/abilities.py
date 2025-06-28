3aa20c5e71506ea6ac7ef6c6fad8dae6ffb47eba
unknown <marwilsondc1204@gmail.com>
2025-06-28 10:21:46 +0800

    Added __init__ method to class Abilities

--- classes/abilities.py
+++ classes/abilities.py
@@ -2,4 +2,10 @@ from math import ceil
 import random
 
 class Abilities:
-    pass
+    def __init__(self, name, power = 20, mana_req = 5, num_hits = 1):
+        self.name = name
+        self.power = power
+        self.mana_req = mana_req
+        self.num_hits = num_hits
+
+    
